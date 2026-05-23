a4852960e0072f2602db5a97ce36ff1ab98655bc
Eli-ezer Reuven Ramirez Ruiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2026-05-23 03:47:48 -0400

    chore: adjust mypy python_version and refine SQLAlchemy type annotations

--- src/rut_validator/core/orm/sqlalchemy/schema.py
+++ src/rut_validator/core/orm/sqlalchemy/schema.py
@@ -6,7 +6,7 @@ from typing import Any
 from rut_validator.core.validator import RutValidator
 
 
-class RutSQLAlchemy(TypeDecorator):  # type: ignore
+class RutSQLAlchemy(TypeDecorator):
     """
     Custom SQLAlchemy type for RUT validation and normalization. This type will ensure that any value being stored in the database is a valid RUT and is normalized to a consistent format.
     When retrieving values from the database, it returns the stored normalized string.
@@ -15,7 +15,7 @@ class RutSQLAlchemy(TypeDecorator):  # type: ignore
     impl = String  # Save as VARCHAR/String into the database
     cache_ok = True
 
-    def process_bind_param(self, value: Any, dialect: Dialect) -> str | None:
+    def process_bind_param(self, value: str | None, dialect: Dialect) -> str | None:
         """
         the `process_bind_param` method is called when a value is being bound to a parameter in a SQL statement. This is where we can validate and normalize the RUT value before it gets sent to the database.
         If the value is None, we can return it directly (or we could choose to return an empty string if we prefer). If the value is already a RutStr instance, we can return its normalized form directly.
@@ -37,7 +37,7 @@ class RutSQLAlchemy(TypeDecorator):  # type: ignore
         except Exception as e:
             raise ValueError(str(e))
 
-    def process_result_value(self, value: Any, dialect: Dialect) -> str | None:
+    def process_result_value(self, value: str | None, dialect: Dialect) -> str | None:
         """
         The `process_result_value` method is called when a value is being retrieved from the database.
         This is where we can convert the stored string back into a RutStr object for easier manipulation in Python.
